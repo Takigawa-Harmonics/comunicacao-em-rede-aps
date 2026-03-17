5332371e5d8c98a43e23720c19a86cc2572a9bae
Samuel Nóbrega <samuelnobrega902@gmail.com>
2026-03-17 20:03:29 -0300

    feat: add ef core

--- ComunicacaoEmRedesApi/Infrastructure/Data/ApplicationDbContext.cs
+++ ComunicacaoEmRedesApi/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace ComunicacaoEmRedesApi.Infrastructure.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
 }
