8d0331c71495417c7ff1bb8fd336adbabb743b19
Samuel Nóbrega <samuelnobrega902@gmail.com>
2026-04-09 13:19:11 -0300

    feat: getting started with token

--- ComunicacaoEmRedesApi/Infrastructure/Data/Migrations/20260409161814_TokenEntity.Designer.cs
+++ ComunicacaoEmRedesApi/Infrastructure/Data/Migrations/20260409161814_TokenEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ComunicacaoEmRedesApi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ComunicacaoEmRedesApi.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409161814_TokenEntity")]
+    partial class TokenEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.25");
---
+++ ComunicacaoEmRedesApi/Infrastructure/Data/Migrations/20260409161814_TokenEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComunicacaoEmRedesApi.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TokenEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "session_tokens",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    hash_token = table.Column<string>(type: "TEXT", maxLength: 70, nullable: false),
+                    expires_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    is_revoked = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_session_tokens", x => x.user_id);
+                    table.ForeignKey(
+                        name: "FK_session_tokens_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "session_tokens");
+        }
+    }
+}
